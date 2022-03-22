186de1b69fae68f3f502e0358a54e9f423b7aa53
trumptheduck <trumptheduck@github.com>
2022-03-22 17:08:21 +0700

    change api url

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 
-router.use('/api', require('./api/post.js'));
-router.use('/api', require('./api/order.js'));
-router.use('/api', require('./api/product.js'));
-router.use('/api', require('./api/user.js'));
+router.use('', require('./api/post.js'));
+router.use('', require('./api/order.js'));
+router.use('', require('./api/product.js'));
+router.use('', require('./api/user.js'));
 module.exports = router;
